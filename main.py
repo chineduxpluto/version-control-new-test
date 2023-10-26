a5ca8ee05e398f3d228851533729289b77b67ec1
chineduxpluto <ezeaniobim@gmail.com>
2023-10-26 11:59:06 +0100

    testing new function

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from version control new test import password
+#from version control new test import password
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def my_function(a, b):
+    return a + b
 
 
 # Press the green button in the gutter to run the script.
@@ -16,4 +19,5 @@ print("How are you?")
 print("How are you?")
 # new lines show as green
 # old lines that were changed show as blue
+print(my_function(2, 3))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
